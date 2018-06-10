584f8c5e4e1aa07db9e3a82cf55e6d81d1353f79
Nickoli Londura <londuran@flip3.engr.oregonstate.edu>
2018-06-09 19:17:02 -0700

    improved test code

--- projects/deneckec/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ projects/deneckec/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -29,8 +29,8 @@ public class UrlValidatorTest extends TestCase {
       assertFalse(validator.isValid(null));
       assertTrue(validator.isValid("http://google.com"));
       assertFalse(validator.isValid("welikjweogij://google.com"));
-      //assertFalse(validator.isValid("https://garbwleikkjfwoijeg.com"));
-      //assertFalse(validator.isValid("https://google.weoijfwe"));
+      assertFalse(validator.isValid("https://garbwleikkjfwoijeg.com"));
+      assertFalse(validator.isValid("https://google.weoijfwe"));
    }
    
    
@@ -58,13 +58,13 @@ public class UrlValidatorTest extends TestCase {
 	   		assertFalse(validator.isValid("hasdkfjsdfkjl://google.com"));
 	   		assertFalse(validator.isValid("httpssdfsadgas://bing.com"));
 	   		assertFalse(validator.isValid("https:gsdafdgagsdf//yahoo.com"));
-	   		//assertFalse(validator.isValid("https://fghkjsdklhjlksdf.com"));
-	   		//assertFalse(validator.isValid("https://oregonstate.ksdfhkjsg"));
-	   		//assertFalse(validator.isValid("https:/ksdfhjkshkdfjakg/youtube.com"));
-	   		//assertFalse(validator.isValid("https://facebook.comsdgflksaghjkhgas"));
-	   		//assertFalse(validator.isValid("https://reddit.orgkdghkjhasjkg"));
-	   		//assertFalse(validator.isValid("https://wikipedia"));
-	   		//assertFalse(validator.isValid("https://twitter.gov"));
+	   		assertFalse(validator.isValid("https://fghkjsdklhjlksdf.com"));
+	   		assertFalse(validator.isValid("https://oregonstate.ksdfhkjsg"));
+	   		assertFalse(validator.isValid("https:/ksdfhjkshkdfjakg/youtube.com"));
+	   		assertFalse(validator.isValid("https://facebook.comsdgflksaghjkhgas"));
+	   		assertFalse(validator.isValid("https://reddit.orgkdghkjhasjkg"));
+	   		assertFalse(validator.isValid("https://wikipedia"));
+	   		assertFalse(validator.isValid("https://twitter.gov"));
    }
    //You need to create more test cases for your Partitions if you need to 
    
@@ -73,42 +73,46 @@ public class UrlValidatorTest extends TestCase {
 	   //You can use this function for programming based testing
 	      String[] schemes = {"http","https"};	   
 		  UrlValidator validator = new UrlValidator(schemes);
-		  String[] urls = {"https://google.com","hasdkfjsdfkjl://google.com",
-				  		   "https://youtube.com","httpssdfsadgas://bing.com",
-				  		   "https://facebook.com","https:gsdafdgagsdf//yahoo.com",
-				  		   "https://baidu.com","https://fghkjsdklhjlksdf.com",
-				  		   "https://wikipedia.org","https://oregonstate.ksdfhkjsg",
-				  		   "https://reddit.com","https:/ksdfhjkshkdfjakg/youtube.com",
-				  		   "https://yahoo.com","https://facebook.comsdgflksaghjkhgas",
-				  		   "https://qq.com","https://reddit.orgkdghkjhasjkg",
-				  		   "https://taobao.com","https://wikipedia",
-				  		   "https://google.co.in","https://twitter.gov",
-				  		   "https://amazon.com","amazon.com",
-				  		   "https://tmall.com","https://",
-				  		   "https://twitter.com","httsdfesifhfe.sma",
-				  		   "https://sohu.com","com.google//:http",
-				  		   "https://instagram.com",".com",
-				  		   "https://vk.com",".org",
-				  		   "https://live.com","http//bing.com",
-				  		   "https://jd.com","shttpsklsjalkflkjs.america",
-				  		   "https://sina.com.cn","com.gnib",
-				  		   "https://weibo.com","http://weeeeeee",
-				  		   "https://yandex.ru","htpt:/gheinsdmgje....corn",
-				  		   "https://360.cn","bert.bertieee.gone",
-				  		   "https://google.co.jp","chase.nickoli.com",
-				  		   "https://google.co.uk","theiah;lksf.com",
-				  		   "https://netflix.com","sfjalejglisjglijsliaf",
-				  		   "https://pornhub.com","j",
-				  		   "https://twitch.tv","outisedse.lol",
-				  		   "https://microsoft.com","iamgroottttt",
-				  		   "https://bing.com","loghebks.gov",
-				  		   "https://ebay.com","poeting.yes"};
+		  String[] urls = {"https://google.com","T","hasdkfjsdfkjl://google.com","F",
+				  		   "https://youtube.com","T","httpssdfsadgas://bing.com","F",
+				  		   "https://facebook.com","T","https:gsdafdgagsdf//yahoo.com","F",
+				  		   "https://baidu.com","T","https://fghkjsdklhjlksdf.com","F",
+				  		   "https://wikipedia.org","T","https://oregonstate.ksdfhkjsg","F",
+				  		   "https://reddit.com","T","https:/ksdfhjkshkdfjakg/youtube.com","F",
+				  		   "https://yahoo.com","T","https://facebook.comsdgflksaghjkhgas","F",
+				  		   "https://qq.com","T","https://reddit.orgkdghkjhasjkg","F",
+				  		   "https://taobao.com","T","https://wikipedia","F",
+				  		   "https://google.co.in","T","https://twitter.gov","F",
+				  		   "https://amazon.com","T","amazon.com","F",
+				  		   "https://tmall.com","T","https://","F",
+				  		   "https://twitter.com","T","httsdfesifhfe.sma","F",
+				  		   "https://sohu.com","T","com.google//:http","F",
+				  		   "https://instagram.com","T",".com","F",
+				  		   "https://vk.com","T",".org","F",
+				  		   "https://live.com","T","http//bing.com","F",
+				  		   "https://jd.com","T","shttpsklsjalkflkjs.america","F",
+				  		   "https://sina.com.cn","T","com.gnib","F",
+				  		   "https://weibo.com","T","http://weeeeeee","F",
+				  		   "https://yandex.ru","T","htpt:/gheinsdmgje....corn","F",
+				  		   "https://360.cn","T","bert.bertieee.gone","F",
+				  		   "https://google.co.jp","T","chase.nickoli.com","F",
+				  		   "https://google.co.uk","T","theiah;lksf.com","F",
+				  		   "https://netflix.com","T","sfjalejglisjglijsliaf","F",
+				  		   "https://msn.com","T","msn","F",
+				  		   "https://twitch.tv","T","outisedse.lol","F",
+				  		   "https://microsoft.com","T","iamgroottttt","F",
+				  		   "https://bing.com","T","loghebks.gov","F",
+				  		   "https://ebay.com","T","poeting.yes","F"};
 		  
 		  Random rand = new Random();
 		  
 		  for(int i= 0; i < 100; i++) {
 			  int  n = rand.nextInt(60);
-			  validator.isValid(urls[n]);
+			  if(n % 2 == 0) {
+				  assertTrue(validator.isValid(urls[n]));
+			  }else {
+				  assertFalse(validator.isValid(urls[n]));
+			  }
 		  }
    }
    
